ef5f88105c74c5e84a537abf510ea78660fe55fa
jpnsantoss <jpnsantos14@gmail.com>
2023-04-10 03:49:31 +0100

    Final.

---
+++ pages/api/favorite/index.ts
@@ -0,0 +1,45 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { without } from "lodash";
+
+import prismadb from '@/lib/prismadb';
+import serverAuth from "@/lib/serverAuth";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+  if (req.method !== 'POST') {
+    return res.status(405).end();
+  }
+  try {
+    const { currentUser } = await serverAuth(req, res);
+
+    const { movieId } = req.body;
+
+    const existingMovie = await prismadb.movie.findUnique({
+      where: {
+        id: movieId,
+      }
+    });
+
+    if (!existingMovie) {
+      throw new Error('Invalid ID');
+    }
+
+    const user = await prismadb.user.update({
+      where: {
+        email: currentUser.email || '',
+      },
+      data: {
+        favoriteIds: {
+          push: movieId
+        }
+      }
+    });
+
+    return res.status(200).json(user);
+
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).end();
+  }
+}
